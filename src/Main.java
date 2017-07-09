66ba178887813e95b86d0695b5e1a7e73c8fc5d5
MattMal <tmalumi@gmail.com>
2017-07-08 22:30:38 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,8 @@
-import java.util.Scanner;
 
-class Processor extends Thread{
-	private volatile boolean running = true;		// Adding volatile makes the value of running to always be read through the class
-	public void run(){
-		
-		while(running){
-			System.out.println("Hello");
-			try {
-				Thread.sleep(300);
-			} catch (InterruptedException e) {}
-		}
-	}
-	
-	public void shutdown(){
-		running  = false;
-	}
-}
 public class Main {
 
-	public static void main(String[] args) throws InterruptedException {
-		Processor p1 = new Processor();
-		p1.start();
-
-		System.out.println("Press return to stop....");
-		Scanner scanner = new Scanner(System.in);
-		scanner.nextLine();
-		
-		p1.shutdown();
+	public static void main(String[] args) {
+		new Worker().main();
 
 	}
 
